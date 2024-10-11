816fa4f716ffa94b3335ebbff2cc455a563cc2c6
kkawayuki <kyukiokawa@gmail.com>
2024-10-10 17:59:01 -0700

    3

--- lab20.cpp
+++ lab20.cpp
@@ -13,21 +13,21 @@ private:
     double *prices; // because prices represents array
 
 public:
-
-    // constructors
+   
     Chair() // default constructor
     { 
         prices = new double[SIZE];
         legs = (rand()%2)+SIZE; //random int variable from 3-4
         for (int i = 0; i < SIZE; i++)
-            prices[i] = 0;
+            prices[i] = (rand()%(MAX-MIN+1) + MIN)/100.00; //random double value from 100.00 to 999.99
     }
-    Chair(int l) // constructor if given parameter
+
+    Chair(int l, double *ptrArr) //take in legs, pointer to array of 3 doubles
     { 
         prices = new double[SIZE];
         legs = l;
         for (int i = 0; i < SIZE; i++)
-            prices[i] = 0;
+            prices[i] = *(ptrArr+i); //iterate through passed array dereferencing each to copy values. 
     }
 
     // setters
@@ -67,7 +67,17 @@ public:
 
 int main()
 {
+    srand(time(0)); //seed random variable
     cout << fixed << setprecision(2);
+
+    Chair *test1 = new Chair; //create first chair obj + its pointer using default (random) constructor
+    test1->print(); // print randomly generated prices and legs
+
+    Chair *test2 = new Chair; //create+print second to show differences in rand values
+    test2->print(); 
+
+    //creating a 
+
 
     // // creating pointer to first chair object
     // Chair *chairPtr = new Chair;
@@ -76,7 +86,7 @@ int main()
     // chairPtr->print();
 
     // // creating dynamic chair object with constructor
-    // Chair *livingChair = new Chair(3);
+    // Chair *livingChair = new Chair();
     // livingChair->setPrices(525.25, 434.34, 252.52);
     // livingChair->print();
     // delete livingChair;
@@ -92,14 +102,6 @@ int main()
     // collection[2].setPrices(626.26, 515.15, 757.57);
     // for (int i = 0; i < SIZE; i++)
     //     collection[i].print();
-
-    srand(time(0));
-
-    
-
-    cout << (rand()%2)+SIZE << '\n'; //random 3 or 4
-
-    cout << fixed << setprecision(2) << (rand()%(MAX-MIN+1) + MIN)/100.00; //random double within 100.00 to 999.99
 
     return 0;
 }
